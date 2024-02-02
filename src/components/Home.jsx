95b5f6dfca371b74b9e918025f95fd8257271ad8
Akash Pawar <akashgp1643pawar+git@gmail.com>
2024-02-02 08:59:22 +0530

    fix mobile ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,18 +33,18 @@ export default function Home() {
                         <h1 className='text-white text-center'>Users</h1>
                         {users.map((user) => (
                             <Link to={`/profile/${user.id}`} className='text-decoration-none text-white'>
-                                <div key={user.profile.username} className="nearby-user border-top border-bottom align-items-center p-4 ">
+                                <div key={user.profile.username} className="nearby-user border-top border-bottom align-items-center p-3 ">
                                     <div className="row">
-                                        <div className="col-md-2 col-sm-2">
+                                        <div className="col-2 col-md-2 p-1">
                                             <img src={user.avatar} onError={addDefaultSrc} className='rounded-circle' height={"100%"} width={"100%"} alt={user.profile.firstName} />
                                         </div>
-                                        <div className="col-md-7 col-sm-7 d-flex align-items-center">
+                                        <div className="col-7 col-md-7 d-flex align-items-center">
                                             <div className=''>
                                                 <h5>{user.profile.firstName} {user.profile.lastName}</h5>
                                                 <p className='mb-0'>{user.jobTitle}</p>
                                             </div>
                                         </div>
-                                        <div className="col-md-3 col-sm-3 d-flex align-items-center">
+                                        <div className="col-3 col-md-3 d-flex align-items-center">
                                             <button className="btn btn-primary pull-right">Details</button>
                                         </div>
                                     </div>
